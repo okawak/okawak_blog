2a13a7ea465ffd1cb5b91e706ea7913d43f3c685
okawak <kodak2022@outlook.jp>
2025-04-07 15:24:57 +0900

    add: git hooks to formatting

--- crates/blog_server/src/main.rs
+++ crates/blog_server/src/main.rs
@@ -1,12 +1,11 @@
-
 #[cfg(feature = "ssr")]
 #[tokio::main]
 async fn main() {
     use axum::Router;
+    use blog_server::app::*;
     use leptos::logging::log;
     use leptos::prelude::*;
-    use leptos_axum::{generate_route_list, LeptosRoutes};
-    use blog_server::app::*;
+    use leptos_axum::{LeptosRoutes, generate_route_list};
 
     let conf = get_configuration(None).unwrap();
     let addr = conf.leptos_options.site_addr;
