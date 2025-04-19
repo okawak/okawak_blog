52c3ec0c44359be1522574e5432f0f9a615006c1
okawak <kodak2022@outlook.jp>
2025-04-19 18:11:25 +0900

    mod: blog_server, still have compile error related wasm

--- crates/blog_server/src/main.rs
+++ crates/blog_server/src/main.rs
@@ -4,9 +4,9 @@ async fn main() {
     use axum::Router;
     use blog_server::app::App;
     use blog_server::app::*;
-    use chrono::format;
+    use blog_server::setup_logging;
+    use leptos::logging::log;
     use leptos::prelude::{provide_context, *};
-    use leptos::{logging::log, server_fn::response};
     use leptos_axum::{LeptosRoutes, generate_route_list};
 
     // initialize the logger
@@ -17,13 +17,11 @@ async fn main() {
     // 認証情報は環境変数または~/.aws/credentialsから自動的に読み込まれます
     let aws_config = aws_config::from_env().load().await;
     let s3_client = aws_sdk_s3::Client::new(&aws_config);
-
-    // アプリケーションの状態としてS3クライアントを保存するための準備
-    let s3_resource = leptos::create_server_resource(move || s3_client.clone());
+    let s3_client_ctx = s3_client.clone();
 
     let conf = get_configuration(None).unwrap();
     let addr = conf.leptos_options.site_addr;
-    let leptos_options = conf.leptos_options;
+    let leptos_options = conf.leptos_options.clone();
 
     // Generate the list of routes in your Leptos App
     let routes = generate_route_list(App);
@@ -31,12 +29,11 @@ async fn main() {
     // Axumルーターを設定
     let app = Router::new()
         .leptos_routes_with_handler(
-            &leptos_options,
             routes,
             {
                 let leptos_options = leptos_options.clone();
-                move || {
-                    provide_context(s3_resource.clone());
+                move |cx| {
+                    provide_context(s3_client_ctx.clone());
                     shell(leptos_options.clone())
                 }
             },
@@ -48,16 +45,18 @@ async fn main() {
                 response
             },
         )
-        .fallback(leptos_axum::file_and_error_handler(move || {
-            provide_context(s3_resource.clone());
-            shell(leptos_options.clone())
-        }))
+        .fallback(leptos_axum::file_and_error_handler_with_context(
+            move |req| {
+                provide_context(s3_client_ctx.clone());
+                shell(leptos_options.clone())(req)
+            },
+        ))
         .with_state(leptos_options);
 
     // run our app with hyper
     // `axum::Server` is a re-export of `hyper::Server`
     log::info!("listening on http://{}", &addr);
-    log!("listening on http://{}", &addr);
+
     let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
     axum::serve(listener, app.into_make_service())
         .await
