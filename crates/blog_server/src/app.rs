3dd569a74009c4aebf449198f5ab41978ed5f447
okawak <kodak2022@outlook.jp>
2025-04-26 12:25:46 +0900

    add: stylance cli, removed tailwindcss

--- crates/blog_server/src/app.rs
+++ crates/blog_server/src/app.rs
@@ -15,6 +15,10 @@ pub fn shell(options: LeptosOptions) -> impl IntoView {
             <head>
                 <meta charset="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link
+                    rel="stylesheet"
+                    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+                />
                 <AutoReload options=options.clone() />
                 <HydrationScripts options />
                 <MetaTags />
