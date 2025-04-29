2acd5140aa596181ddb7b1ca3ff81eb073f6d61b
okawak <kodak2022@outlook.jp>
2025-04-29 15:44:59 +0900

    fix: equation format

--- crates/notion_api/src/markdown/paragraph.rs
+++ crates/notion_api/src/markdown/paragraph.rs
@@ -44,34 +44,33 @@ pub fn process(value: &Value) -> Result<String, Box<dyn Error>> {
             }
         } else if let Some(equation) = element.get("equation") {
             let expression = equation.get("expression").unwrap().to_string();
-            // 装飾も可能だが、数式には適用しない
-            // let annotations = element.get("annotations").unwrap();
-            // let is_bold = annotations.get("bold").unwrap().as_bool().unwrap();
-            // let is_italic = annotations.get("italic").unwrap().as_bool().unwrap();
-            // let is_underline = annotations.get("underline").unwrap().as_bool().unwrap();
-            // let is_strikethrough = annotations.get("strikethrough").unwrap().as_bool().unwrap();
-            // let is_code = annotations.get("code").unwrap().as_bool().unwrap();
-            // let _is_color = annotations.get("color").unwrap().as_str().unwrap();
+            let annotations = element.get("annotations").unwrap();
+            let is_bold = annotations.get("bold").unwrap().as_bool().unwrap();
+            let is_italic = annotations.get("italic").unwrap().as_bool().unwrap();
+            let is_underline = annotations.get("underline").unwrap().as_bool().unwrap();
+            let is_strikethrough = annotations.get("strikethrough").unwrap().as_bool().unwrap();
+            let is_code = annotations.get("code").unwrap().as_bool().unwrap();
+            let _is_color = annotations.get("color").unwrap().as_str().unwrap();
 
             let mut content = expression.trim_matches('"').to_string();
             content = content.replace("\\\\", "\\");
             content = format!("${content}$");
             // 装飾を適用
-            // if is_bold {
-            //     content = format!("**{}**", content);
-            // }
-            // if is_italic {
-            //     content = format!("_{}_", content);
-            // }
-            // if is_underline {
-            //     content = format!("<u>{}</u>", content);
-            // }
-            // if is_strikethrough {
-            //     content = format!("~~{}~~", content);
-            // }
-            // if is_code {
-            //     content = format!("`{}`", content);
-            // }
+            if is_bold {
+                content = format!(" **{content}** ");
+            }
+            if is_italic {
+                content = format!(" _{content}_ ");
+            }
+            if is_underline {
+                content = format!("<u>{content}</u>");
+            }
+            if is_strikethrough {
+                content = format!(" ~~{content}~~ ");
+            }
+            if is_code {
+                content = format!("`{content}`");
+            }
 
             paragraph.push_str(&content);
         }
